f85269f55b5959528e34a7335213dce2f7dfb018
Piyush Raj <piyushraj26102004@gmail.com>
2026-04-11 21:32:10 +0530

    added hero section

---
+++ apps/web/src/components/Navbar/LandingNavbar.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useEffect, useState, useCallback } from "react";
+import { Button } from "@base-ui/react";
+import { cn } from "../../../lib/utils";
+import { Exo_2 } from "next/font/google";
+
+const exo2 = Exo_2({
+  subsets: ["latin"],
+  weight: ["800"],
+  style: ["italic"],
+  display: "swap",
+});
+
+export default function LandingNavbar() {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  const handleScroll = useCallback(() => {
+    if (window.scrollY > 80) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
+  return (
+    <nav
+      className={cn(
+        "h-18 w-full fixed top-0 left-0 flex items-center justify-between px-8 text-[12px] font-sans z-50 transition-all duration-500",
+        isScrolled
+          ? "bg-dark-alpha backdrop-blur-xl  text-light-base"
+          : "bg-transparent text-light-base/80",
+      )}
+    >
+      <div className="flex gap-x-8 items-center">
+        {/* <span
+          className={cn(
+            'text-light-base leading-none pb-1 transition-transform duration-300',
+            exo2.className,
+            isScrolled ? "scale-95" : "scale-100"
+          )}
+        >
+          SOLMARKET
+        </span> */}
+        <div className="flex gap-x-6">
+          {["SOLMARKET", "ABOUT", "RESOURCES", "ECOSYSTEM"].map((item) => (
+            <span
+              key={item}
+              className="cursor-pointer hover:text-light-base transition-colors duration-150"
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex items-center">
+        <Button
+          className={cn(
+            "bg-light-base text-dark-base px-6 py-2 rounded-full font-sans font-medium hover:opacity-90 transition-all",
+            isScrolled ? "py-1.5 text-[11px]" : "py-2",
+          )}
+        >
+          LOGIN
+        </Button>
+      </div>
+    </nav>
+  );
+}
---
+++ apps/web/src/components/utility/SectionDivider.tsx
@@ -0,0 +1,38 @@
+import { cn } from "../../../lib/utils";
+import { robotoCondensed } from "../Hero/LandingHeroSection";
+
+export default function SectionDivider() {
+  const MarqueeItem = () => (
+    <>
+      <div className="text-2xl text-dark-base uppercase font-semibold whitespace-nowrap">
+        SOLANA DEDICATED <span className="font-serif font-normal italic">PREDICTION MARKET</span>
+      </div>
+      <div className="h-px w-20 bg-dark-base shrink-0" />
+    </>
+  );
+
+  return (
+    <div className="h-20 w-full bg-light-base overflow-hidden flex items-center border-y border-dark-base/10">
+      <div
+        className={cn(
+          "flex items-center gap-x-10 animate-marquee w-max",
+          robotoCondensed.className,
+        )}
+      >
+        <div className="flex items-center gap-x-10">
+          <MarqueeItem />
+          <MarqueeItem />
+          <MarqueeItem />
+          <MarqueeItem />
+        </div>
+
+        <div className="flex items-center gap-x-10">
+          <MarqueeItem />
+          <MarqueeItem />
+          <MarqueeItem />
+          <MarqueeItem />
+        </div>
+      </div>
+    </div>
+  );
+}
