9cfd18e9ab1172812176080f0e9f21bc44ee4221
kant-github <kantrishi7779@gmail.com>
2026-05-10 18:48:02 +0530

    fixed footer

--- apps/web/src/components/hero/LandingFooter.tsx
+++ apps/web/src/components/hero/LandingFooter.tsx
@@ -20,7 +20,7 @@ interface FooterGroup {
 
 export default function LandingFooter(): JSX.Element {
     return (
-        <footer className="relative w-full bg-white pt-24 pb-10 px-6 md:px-10">
+        <footer className="relative w-full bg-white pt-40 md:pt-48 pb-10 px-6 md:px-10">
             <div className="max-w-340 mx-auto w-full">
                 <BrandRow />
                 <div className="my-16 h-px bg-black/10" />
